4d523ac03f36e8dca0be003904ad6bd4b7259e6b
jkittel <jonkittel@gmail.com>
2024-12-19 00:37:38 -0500

    game class made

--- game_mechanics.py
+++ game_mechanics.py
@@ -3,13 +3,8 @@ import random
 import numpy as np
 
 
-x_width = 20
-y_height = 20
-mine_min = 50
-mine_max = 100
 
-rows = 20
-cols = 20
+
 
 class Cell:
     def __init__(self, mine=False, show=False, score=0, mark=0):
@@ -22,141 +17,173 @@ class Cell:
         return f"Cell({self.mine},{self.show},{self.score},{self.mark})"
 
 
-map_array_1 = [[Cell() for _ in range(cols)] for _ in range (rows)]
+class Game:
 
-mine_count = random.randint(50,100)
-
-
-
-def check_neighbors(x,y):
-    score = 0
-    score += local_check((x-1), (y-1))
-    score += local_check(x, (y-1))
-    score += local_check((x+1), (y-1))
-
-    score += local_check((x-1), y)
-    score += local_check((x+1), y)
-
-    score += local_check((x-1), (y+1))
-    score += local_check(x, (y+1))
-    score += local_check((x+1), (y+1))
-
-    return score
-    
-
-def local_check(x,y):
-    if x < 0 or x > (x_width -1):
-        return False
-    if y < 0 or y > (y_height -1):
-        return False
-    if map_array_1[x][y].mine == True:
-        return True
-    else:
-        return False
-    
-
-def fill_map(mine_count):
-    i = 0
-    while i < mine_count:
-        x_random = random.randint(0,(x_width - 1))
-        y_random = random.randint(0,(y_height - 1))
-        if map_array_1[x_random][y_random].mine == False:
-            map_array_1[x_random][y_random].mine = True
-            i += 1
+    def __init__(self, difficulty):
+        match difficulty:
+            case 0:
+                print("easy")
+            case 1:
+                print("medium")
+            case 2:
+                print("hard")
+            case _:
+                print("default")
 
 
-def setup_maps():
-    print("for map cell array: x ", x_width, " y", y_height)
+        self.x_width = 20
+        self.y_height = 20
+        self.mine_min = 50
+        self.mine_max = 100
 
-    mine_count = random.randint(mine_min,mine_max)
+        self.rows = 20
+        self.cols = 20
 
-    fill_map(mine_count)
-
-    for j in range(y_height):
-        for i in range(x_width):
-            map_array_1[i][j].score = check_neighbors(i,j)
+        self.map_array_1 = [[Cell() for _ in range(self.cols)] for _ in range (self.rows)]
+        self.mine_count = random.randint(50,100)
 
 
-def print_map_details():
-    for j in range(y_height):
-        for i in range(x_width):
-            if map_array_1[i][j].mine == True:
-               print("x", end="")
+
+    def check_neighbors(self, x,y):
+        score = 0
+        score += self.local_check((x-1), (y-1))
+        score += self.local_check(x, (y-1))
+        score += self.local_check((x+1), (y-1))
+
+        score += self.local_check((x-1), y)
+        score += self.local_check((x+1), y)
+
+        score += self.local_check((x-1), (y+1))
+        score += self.local_check(x, (y+1))
+        score += self.local_check((x+1), (y+1))
+
+        return score
+        
+
+    def local_check(self, x,y):
+        if x < 0 or x > (self.x_width -1):
+            return False
+        if y < 0 or y > (self.y_height -1):
+            return False
+        if self.map_array_1[x][y].mine == True:
+            return True
+        else:
+            return False
+        
+
+    def fill_map(self, mine_count):
+        i = 0
+        while i < mine_count:
+            x_random = random.randint(0,(self.x_width - 1))
+            y_random = random.randint(0,(self.y_height - 1))
+            if self.map_array_1[x_random][y_random].mine == False:
+                self.map_array_1[x_random][y_random].mine = True
+                i += 1
+
+
+    def setup_maps(self):
+        print("for map cell array: x ", self.x_width, " y", self.y_height)
+
+        mine_count = random.randint(self.mine_min,self.mine_max)
+
+        self.fill_map(mine_count)
+
+        for j in range(self.y_height):
+            for i in range(self.x_width):
+                self.map_array_1[i][j].score = self.check_neighbors(i,j)
+
+
+    def print_map_details(self):
+        for j in range(self.y_height):
+            for i in range(self.x_width):
+                if self.map_array_1[i][j].mine == True:
+                    print("x", end="")
+                else:
+                    print("-", end="")
+            print(" |")
+
+        print(" finished printing map with locations")
+        print("")
+
+        for j in range(self.y_height):
+            for i in range(self.x_width):
+                score = self.check_neighbors(i,j)
+                self.map_array_1[i][j].score = score
+                print(score, end="")
+            print(" |")
+
+
+
+
+    def print_map(self):
+        for j in range(self.y_height):
+            for i in range(self.x_width):
+                if self.map_array_1[i][j].show == True:
+                    print(self.map_array_1[i][j].score, end="")
+                else:
+                    print("-", end="")
+            print(" |")
+
+
+
+    def reveal_spread(self, x,y,counter):
+        if x < 0 or x >= self.x_width:
+            return
+        if y < 0 or y >= self.y_height:
+            return
+        counter -= 1
+        if counter <= 0:
+            return
+        if self.map_array_1[x][y].show == False:
+            if self.map_array_1[x][y].mine == False:
+                self.map_array_1[x][y].show = True
+                self.reveal_spread(x-1,y, counter)
+                self.reveal_spread(x+1,y, counter)
+                self.reveal_spread(x,y-1, counter)
+                self.reveal_spread(x,y+1, counter)
+
+
+
+
+    def game_loop(self):
+        player_lives = 2
+        while(player_lives > 0):
+            self.print_map()
+            number_valid = False
+            while number_valid == False:
+                x_move = int(input("x: "))
+                y_move = int(input("y: "))
+                print("x is : ", x_move, "y is :", y_move)
+                if x_move >= 0 and x_move < self.x_width:
+                    if y_move >= 0 and y_move < self.y_height:
+                        number_valid = True
+            
+            if self.map_array_1[x_move][y_move].mine == True:
+                print("player lost")
+                exit()
             else:
-                print("-", end="")
-        print(" |")
+                #reveal_array[x_move][y_move] = True
+                print("good choice")
+                self.reveal_spread(x_move,y_move,4)
 
-    print("")
-
-    for j in range(y_height):
-        for i in range(x_width):
-            score = check_neighbors(i,j)
-            map_array_1[i][j].score = score
-            print(score, end="")
-        print(" |")
-
-
-
-
-def print_map():
-    for j in range(y_height):
-        for i in range(x_width):
-            if map_array_1[i][j].show == True:
-                print(map_array_1[i][j].score, end="")
-            else:
-                print("-", end="")
-        print(" |")
-
-
-
-def reveal_spread(x,y,counter):
-    if x < 0 or x >= x_width:
-        return
-    if y < 0 or y >= y_height:
-        return
-    counter -= 1
-    if counter <= 0:
-        return
-    if map_array_1[x][y].show == False:
-        if map_array_1[x][y].mine == False:
-            map_array_1[x][y].show = True
-            reveal_spread(x-1,y, counter)
-            reveal_spread(x+1,y, counter)
-            reveal_spread(x,y-1, counter)
-            reveal_spread(x,y+1, counter)
 
 def main():
 
-    print("x ", x_width, " y", y_height)
+    input_check = False
+    while input_check == False:
+        user_input = int(input("choose difficulty 0 easy 1 medium 2 hard:"))
+        if 0 <= user_input <= 2:
+            input_check = True
 
-    setup_maps()
+    new_game = Game(user_input)
 
-    print_map_details()
+    new_game.setup_maps()
 
-    game_loop()
+    new_game.print_map_details()
 
-
-def game_loop():
-    player_lives = 2
-    while(player_lives > 0):
-        print_map()
-        number_valid = False
-        while number_valid == False:
-            x_move = int(input("x: "))
-            y_move = int(input("y: "))
-            print("x is : ", x_move, "y is :", y_move)
-            if x_move >= 0 and x_move < x_width:
-                if y_move >= 0 and y_move < y_height:
-                    number_valid = True
-        
-        if map_array_1[x_move][y_move].mine == True:
-            print("player lost")
-            exit()
-        else:
-            #reveal_array[x_move][y_move] = True
-            print("good choice")
-            reveal_spread(x_move,y_move,4)
+    new_game.game_loop()
 
 
+main()
     
 
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-import game_mechanics
+
 from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
+
+
+GRID_SIZES = {
+    "easy": (5,5),
+    "medium": (10,10),
+    "hard": (15,15)
+}
+
+class Cell:
+    def __init__(self, mine=False, show=False, score=0, mark=0):
+        self.mine = mine
+        self.show = show
+        self.score = score
+        self.mark = mark 
+
+    def __repr__(self):
+        return f"Cell({self.mine},{self.show},{self.score},{self.mark})"
+    
+    def to_dict(self):
+        return {"mine": self.mine, "show": self.show, "score": self.score, "mark": self.mark}
 
 @app.route("/")
 def home():
@@ -23,6 +43,35 @@ def handle_click():
     print(f"Received click at Row: {row}, Column: {column}")
 
     return jsonify({"status": "success", "row":row, "column":column})
+
+
+@app.route("/start_game", methods=["POST"])
+def start_game():
+    data = request.get_json()
+    difficulty = data.get("difficulty", "easy")
+    rows, cols = GRID_SIZES.get(difficulty, (5,5))
+
+    grid = [[Cell() for _ in range(cols)] for _ in range (rows)]
+    app.config["grid"] = grid
+
+    return jsonify({"rows": rows, "colums": cols})
+
+
+def update_cell():
+    grid = app.config.get("grid", [])
+    data = request.get_json()
+    row, col =  data.get("row"), data.get("col")
+
+    if 0 <= row < len(grid)and 0 <= col < len(grid[0]):
+
+        cell = grid[row][col]
+        cell.show = True
+        cell.score += 1
+
+        return jsonify(cell.to_dict())
+    else:
+        return jsonify({"error": "Invalid cell coordinates"}), 400
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
